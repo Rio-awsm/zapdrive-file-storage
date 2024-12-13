a3a107ba4ef752018f31647acb52824769b41507
Supriyo Saha <mm.saha1982@gmail.com>
2024-12-14 04:34:26 +0530

    style::reusable auth form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,11 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>
-      signin
-    </div>
+    <>
+      <AuthForm type="sign-in"/>
+    </>
   )
 }
 
