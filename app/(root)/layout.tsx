704a85b68477fc5d37a57ef94362019f656275d6
Supriyo Saha <mm.saha1982@gmail.com>
2024-12-18 02:09:16 +0530

    mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -18,7 +18,7 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
         email={currentUser.email}
       />
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
+        <MobileNavigation {...currentUser} />
         <Header />
         <div className="main-content">{children}</div>
       </section>
